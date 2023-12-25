b8f18467831e4c7cb5f2aa7dc00e5e8572451ad2
Diana Gromova <16adianay@gmail.com>
2023-12-25 17:39:11 +0400

    ASP.NET example

--- ASP.NET Core/Controllers/SampleDataController.cs
+++ ASP.NET Core/Controllers/SampleDataController.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
 using ASP_NET_Core.Models;
 using DevExtreme.AspNet.Data;
 using DevExtreme.AspNet.Mvc;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ASP_NET_Core.Controllers {
 
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     public class SampleDataController : Controller {
-
+       
         [HttpGet]
         public object Get(DataSourceLoadOptions loadOptions) {
-            return DataSourceLoader.Load(SampleData.Orders, loadOptions);
+            return DataSourceLoader.Load(SampleData.Customers, loadOptions);
         }
+        [HttpPut]
+        public IActionResult Put(int key, string values)
+        {
 
+            var item = SampleData.Customers.First(e => e.ID == key);
+
+            JsonConvert.PopulateObject(values, item);
+
+            if (!TryValidateModel(item))
+                return BadRequest(ModelState);
+
+            return Ok(item);
+        }
     }
 }
--- ASP.NET Core/Models/SampleOrder.cs
+++ ASP.NET Core/Models/SampleOrder.cs
@@ -1,16 +1,34 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ASP_NET_Core.Models {
-    public class SampleOrder {
-        public int OrderID { get; set; }
-        public DateTime OrderDate { get; set; }
-        public string CustomerID { get; set; }
-        public string CustomerName { get; set; }
-        public string ShipCountry { get; set; }
-        public string ShipCity { get; set; }
+    public class Customer
+    {
+        
+        public int ID { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+
+        public string Address { get; set; }
+
+        [StringLength(int.MaxValue, MinimumLength = 4, ErrorMessage = "City must have at least 2 symbols")]
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public int Zipcode { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\(\d{3}\)\ \d{3}-\d{4}$", ErrorMessage = @"Your phone must have ""(555) 555-5555"" format!")]
+        public string Phone { get; set; }
+
+        public string Fax { get; set; }
+
+        public string Website { get; set; }
     }
 }
